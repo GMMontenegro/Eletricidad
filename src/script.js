196b2f901ee0d9102e5558a9672d9efadf282bb7
GMMontenegro <gabriel.montenegro@sinqia.com.br>
2023-11-29 09:25:31 -0300

    Ajustes

--- src/script.js
+++ src/script.js
@@ -9,12 +9,35 @@ function calc() {
     let xl = 2 * Math.PI * f * l;
     let xc = 1 / (2 * Math.PI * f * c);
     console.log(f, l, c, xl, xc);
-    document.getElementById("result").innerHTML = "XL: "+xl+" XC: "+xc;
+
+    let casa = parseInt(document.getElementById("casa").value);
+
+    let sxc = subStr(xc.toString(), casa);
+    let sxl = subStr(xl.toString(), casa);
+
+    const result = document.querySelector("#result")
+
+    let value = "XL: "+sxl+" XC: "+sxc;
+
+    result.value = value;
+    // document.getElementById("result").value = "XL: "+xl+" XC: "+xc;
 }
+
+function subStr(str, casa) {
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === ".") {
+            return str.substring(0, i + casa);
+        }
+    }
+    return str
+}
+
 function clear() {
-    document.getElementById("result").innerHTML = "";
-    document.getElementById("Frecuencia").value = "";
-    document.getElementById("Bobina").value = "";
-    document.getElementById("Condensador").value = "";
+    document.getElementById("Frecuencia").value = 0;
+    document.getElementById("Bobina").value = 0;
+    document.getElementById("Condensador").value = 0;
+    const result = document.querySelector("#result")
+    result.value = "";
 
 }
